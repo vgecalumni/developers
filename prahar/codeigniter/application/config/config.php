327fc0dc205ac392ac8fccc9863186dabd10e36a
prahar4954 <34216391+prahar4954@users.noreply.github.com>
2019-05-29 23:08:53 +0530

    MVC Enhancement

--- prahar/codeigniter/application/config/config.php
+++ prahar/codeigniter/application/config/config.php
@@ -378,12 +378,18 @@ $config['encryption_key'] = '';
 |
 */
 $config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
-$config['sess_match_ip'] = FALSE;
-$config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
+
+ $config['sess_cookie_name'] = 'ci_session';
+ $config['sess_expiration'] = 7200;
+ $config['sess_expire_on_close'] = FALSE;
+ $config['sess_encrypt_cookie'] = TRUE;  // This must be TRUE
+ $config['sess_use_database'] = FALSE; // This must be FALSE
+ $config['sess_table_name'] = 'ci_sessions';
+ $config['sess_match_ip'] = FALSE;
+ $config['sess_match_useragent'] = TRUE;
+ $config['sess_time_to_update'] = 300;
 
 /*
 |--------------------------------------------------------------------------
---
+++ prahar/codeigniter/application/views/layout/admin_panel/breadcrumb.php
@@ -0,0 +1,13 @@
+<div class="row" style="position: fixed;width:100%;z-index: 2;margin-top: -1.5rem;margin-left:-1.7rem;margin-right:-1.7rem;z-index: 500;background: #fff">
+
+            <div class="col-12" style="padding:0;">
+              <span class="d-block d-md-flex align-items-center" style="padding:0;border-bottom: 1px solid #dadce0;">
+                <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+
+                  <?php  echo $this->breadcrumb->output(); ?>
+                </ol>
+                </nav>
+              </span>
+            </div>
+          </div>
