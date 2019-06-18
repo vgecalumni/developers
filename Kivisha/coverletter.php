a680aad1d1101de6d7559bcbd758f827191ed8ec
kivishashah <51192282+kivishashah@users.noreply.github.com>
2019-06-18 22:37:20 +0530

    Create coverletter.php

---
+++ Kivisha/coverletter.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<body>
+<div style="margin:0px 600px "> 
+<br>
+<br>
+<h2><center><u> COVER LETTER </u></center></h2>
+<div style="height:50px;width:70px;float:left;font-weight:bold;font-size:17px">
+Subject:
+</div>
+<div style="height:40px; width:100px;float:left">
+<input style="margin-left:50px"
+				type="text" name="sub"><br>
+				</div>
+				<div style="clear:left"></div>
+<div style="height:60px;width:70px;float:left;font-weight:bold;font-size:17px">
+Proposal:
+</div>
+<div style="height:40px; width:100px;float:left">
+<textarea rows="3" col="3" style="margin-left:50px" ></textarea><br>
+</div>
+<div style="clear:left"></div>
+<h2><center><u> EVENT DETAILS </u></center></h2>
+<div style="height:50px;width:70px;float:left;font-weight:bold;font-size:17px">
+EventName:
+</div>
+<div style="height:40px; width:100px;float:left">
+<input style="margin-left:50px"
+				type="text" name="sub"><br>
+				</div>
+				<div style="clear:left"></div>
+<div style="height:70px;width:70px;float:left;font-weight:bold;font-size:17px">
+Need For the Event:
+</div>
+<div style="height:40px; width:100px;float:left">
+<textarea rows="3" col="3" style="margin-left:50px" ></textarea><br>
+				</div>
+				<div style="clear:left"></div>
+<div style="height:70px;width:70px;float:left;font-weight:bold;font-size:17px">
+Event Contents:
+</div>
+<div style="height:40px; width:100px;float:left">
+<textarea rows="3" col="3" style="margin-left:50px" ></textarea><br>
+				</div>
+				<div style="clear:left"></div>				
+<div style="height:70px;width:70px;float:left;font-weight:bold;font-size:17px">
+Event Objective:
+</div>
+<div style="height:40px; width:100px;float:left">
+<textarea rows="3" col="3" style="margin-left:50px" ></textarea><br>
+				</div>
+				<div style="clear:left"></div>
+<div style="height:70px;width:70px;float:left;font-weight:bold;font-size:17px">
+Event Outcome:
+</div>
+<div style="height:40px; width:100px;float:left">
+<textarea rows="3" col="3" style="margin-left:50px" ></textarea><br>
+				</div>
+				<div style="clear:left"></div>		
+				
+<center><input type="button" value="ADD" style="center"></center>
+</div>
+
+<?php
+//echo"hiii";
+?>
