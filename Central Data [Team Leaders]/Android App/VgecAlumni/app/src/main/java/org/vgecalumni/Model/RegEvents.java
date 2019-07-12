23085763101615fc64a4d63eeb611ca3f39a09ef
Nikhil1999 <nikhilkoshty@gmail.com>
2019-07-12 20:32:37 +0530

    Vgec Alumni App

---
+++ Central Data [Team Leaders]/Android App/VgecAlumni/app/src/main/java/org/vgecalumni/Model/RegEvents.java
@@ -0,0 +1,23 @@
+package org.vgecalumni.Model;
+
+public class RegEvents{
+    String event,photo,food;
+
+    public RegEvents(String event, String photo, String food) {
+        this.event = event;
+        this.photo = photo;
+        this.food = food;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public String getFood() {
+        return food;
+    }
+}
